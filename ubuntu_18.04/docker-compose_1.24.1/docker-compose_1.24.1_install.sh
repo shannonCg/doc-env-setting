dde44193fe831e42bff2c923a5b3aa3c5897a056
shannonCg <phy8233@gmail.com>
2019-11-03 12:09:15 +0800

    add install docker-compose 1.24.1 description

---
+++ ubuntu_18.04/docker-compose_1.24.1/docker-compose_1.24.1_install.sh
@@ -0,0 +1,5 @@
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose 
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo curl -L https://raw.githubusercontent.com/docker/compose/1.24.1/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
+
